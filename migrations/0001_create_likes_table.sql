df91d104bdb2fe19b3a6e58cb55516520465d554
justkinou <justkinou@proton.me>
2026-09-17 01:42:17 +0200

    chore: rename table and column names for database operations

--- migrations/0001_create_likes_table.sql
+++ migrations/0001_create_likes_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS post_likes (
     user_id BIGINT NOT NULL,
     post_id BIGINT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-    UNIQUE (user_id, track_id)
+    UNIQUE (user_id, post_id)
 );
--- src/database/likes.rs
+++ src/database/likes.rs
@@ -1,48 +1,48 @@
 use crate::database;
 
 pub trait LikesDb {
-    async fn like_track(&self, user_id: i64, track_id: i64) -> Result<(), sqlx::Error>;
-    async fn dislike_track(&self, user_id: i64, track_id: i64) -> Result<(), sqlx::Error>;
-    async fn get_liked_tracks_for_user(&self, user_id: i64, page_number: i32, page_size: i32) -> Result<Vec<i64>, sqlx::Error>;
-    async fn get_likes_count_for_track(&self, track_id: i64) -> Result<i64, sqlx::Error>;
-    async fn get_liked_tracks_count_for_user(&self, user_id: i64) -> Result<i64, sqlx::Error>;
+    async fn like_post(&self, user_id: i64, post_id: i64) -> Result<(), sqlx::Error>;
+    async fn dislike_post(&self, user_id: i64, post_id: i64) -> Result<(), sqlx::Error>;
+    async fn get_liked_posts_for_user(&self, user_id: i64, page_number: i32, page_size: i32) -> Result<Vec<i64>, sqlx::Error>;
+    async fn get_likes_count_for_post(&self, post_id: i64) -> Result<i64, sqlx::Error>;
+    async fn get_liked_posts_count_for_user(&self, user_id: i64) -> Result<i64, sqlx::Error>;
 }
 
 impl LikesDb for database::SocialDb {
-    async fn like_track(&self, user_id: i64, track_id: i64) -> Result<(), sqlx::Error> {
-        sqlx::query!("INSERT INTO likes (user_id, track_id) VALUES ($1, $2);", user_id, track_id)
+    async fn like_post(&self, user_id: i64, post_id: i64) -> Result<(), sqlx::Error> {
+        sqlx::query!("INSERT INTO post_likes (user_id, post_id) VALUES ($1, $2);", user_id, post_id)
             .execute(&self.pool)
             .await?;
         Ok(())
     }
 
-    async fn dislike_track(&self, user_id: i64, track_id: i64) -> Result<(), sqlx::Error> {
+    async fn dislike_post(&self, user_id: i64, post_id: i64) -> Result<(), sqlx::Error> {
         sqlx::query!(r"
-            DELETE FROM likes
-            WHERE user_id = $1 AND track_id = $2;
-        ", user_id, track_id)
+            DELETE FROM post_likes
+            WHERE user_id = $1 AND post_id = $2;
+        ", user_id, post_id)
             .execute(&self.pool)
             .await?;
         Ok(())
     }
 
-    async fn get_liked_tracks_for_user(&self, user_id: i64, page_number: i32, page_size: i32) -> Result<Vec<i64>, sqlx::Error> {
+    async fn get_liked_posts_for_user(&self, user_id: i64, page_number: i32, page_size: i32) -> Result<Vec<i64>, sqlx::Error> {
         let offset: i32 = (page_number - 1) * page_size;
-        let tracks_ids = sqlx::query_scalar!(r"
-            SELECT track_id FROM likes
+        let posts_ids = sqlx::query_scalar!(r"
+            SELECT post_id FROM post_likes
             WHERE user_id = $1
             ORDER BY created_at DESC
             LIMIT $2 OFFSET $3;
         ", user_id, page_size as i64, offset as i64)
             .fetch_all(&self.pool)
             .await?;
-        Ok(tracks_ids)
+        Ok(posts_ids)
     }
 
-    async fn get_liked_tracks_count_for_user(&self, user_id: i64) -> Result<i64, sqlx::Error> {
+    async fn get_liked_posts_count_for_user(&self, user_id: i64) -> Result<i64, sqlx::Error> {
         let count = sqlx::query_scalar!(r"
             SELECT COUNT(*)
-            FROM likes
+            FROM post_likes
             WHERE user_id = $1;
         ", user_id)
             .fetch_one(&self.pool)
@@ -50,12 +50,12 @@ impl LikesDb for database::SocialDb {
         Ok(count.unwrap_or_default())
     }
 
-    async fn get_likes_count_for_track(&self, track_id: i64) -> Result<i64, sqlx::Error> {
+    async fn get_likes_count_for_post(&self, post_id: i64) -> Result<i64, sqlx::Error> {
         let count: Option<i64> = sqlx::query_scalar!(r"
             SELECT COUNT(user_id)
-            FROM likes
-            WHERE track_id = $1;
-        ", track_id)
+            FROM post_likes
+            WHERE post_id = $1;
+        ", post_id)
             .fetch_one(&self.pool)
             .await?;
         Ok(count.unwrap_or_default())
@@ -68,21 +68,21 @@ use crate::database::likes::LikesDb;
 use super::database::SocialDb;
 
     #[sqlx::test]
-    async fn test_like_track(pool: sqlx::PgPool) -> anyhow::Result<()> {
+    async fn test_like_post(pool: sqlx::PgPool) -> anyhow::Result<()> {
         let db = SocialDb {
             pool,
         };
-        db.like_track(1, 1)
+        db.like_post(1, 1)
             .await?;
         Ok(())
     }
 
     #[sqlx::test]
-    async fn test_dislike_track(pool: sqlx::PgPool) -> anyhow::Result<()> {
+    async fn test_dislike_post(pool: sqlx::PgPool) -> anyhow::Result<()> {
         let db = SocialDb {
             pool,
         };
-        db.dislike_track(1, 1)
+        db.dislike_post(1, 1)
             .await?;
         Ok(())
     }
@@ -93,12 +93,12 @@ use super::database::SocialDb;
             pool,
         };
         tokio::try_join!(
-            db.like_track(1, 1),
-            db.like_track(2, 1),
-            db.like_track(3, 1),
-            db.like_track(4, 1),
+            db.like_post(1, 1),
+            db.like_post(2, 1),
+            db.like_post(3, 1),
+            db.like_post(4, 1),
         )?;
-        let count = db.get_likes_count_for_track(1).await?;
+        let count = db.get_likes_count_for_post(1).await?;
         assert_eq!(count, 4);
         Ok(())
     }
@@ -109,43 +109,43 @@ use super::database::SocialDb;
             pool,
         };
         tokio::try_join!(
-            db.like_track(1, 1),
-            db.like_track(2, 1),
-            db.like_track(3, 1),
-            db.like_track(4, 1),
-            db.dislike_track(1, 1),
-            db.dislike_track(5, 1),
+            db.like_post(1, 1),
+            db.like_post(2, 1),
+            db.like_post(3, 1),
+            db.like_post(4, 1),
+            db.dislike_post(1, 1),
+            db.dislike_post(5, 1),
         )?;
-        let count = db.get_likes_count_for_track(1).await?;
+        let count = db.get_likes_count_for_post(1).await?;
         assert_eq!(count, 3);
         Ok(())
     }
 
     #[sqlx::test]
-    async fn test_get_liked_tracks(pool: sqlx::PgPool) -> anyhow::Result<()> {
+    async fn test_get_liked_posts(pool: sqlx::PgPool) -> anyhow::Result<()> {
         let db = SocialDb {
             pool,
         };
-        db.like_track(1, 1).await?;
-        db.like_track(1, 3).await?;
-        db.like_track(1, 5).await?;
-        db.like_track(1, 4).await?;
-        let tracks = db.get_liked_tracks_for_user(1, 1, 10).await?;
-        assert_eq!(tracks, vec![4, 5, 3, 1]);
+        db.like_post(1, 1).await?;
+        db.like_post(1, 3).await?;
+        db.like_post(1, 5).await?;
+        db.like_post(1, 4).await?;
+        let posts = db.get_liked_posts_for_user(1, 1, 10).await?;
+        assert_eq!(posts, vec![4, 5, 3, 1]);
         Ok(())
     }
 
     #[sqlx::test]
-    async fn test_get_liked_tracks_with_pagination(pool: sqlx::PgPool) -> anyhow::Result<()> {
+    async fn test_get_liked_posts_with_pagination(pool: sqlx::PgPool) -> anyhow::Result<()> {
         let db = SocialDb {
             pool,
         };
-        db.like_track(1, 1).await?;
-        db.like_track(1, 3).await?;
-        db.like_track(1, 5).await?;
-        db.like_track(1, 4).await?;
-        let tracks = db.get_liked_tracks_for_user(1, 2, 2).await?;
-        assert_eq!(tracks, vec![3, 1]);
+        db.like_post(1, 1).await?;
+        db.like_post(1, 3).await?;
+        db.like_post(1, 5).await?;
+        db.like_post(1, 4).await?;
+        let posts = db.get_liked_posts_for_user(1, 2, 2).await?;
+        assert_eq!(posts, vec![3, 1]);
         Ok(())
     }
 }
